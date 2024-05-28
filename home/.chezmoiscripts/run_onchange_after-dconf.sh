b5c9e0cbe328e01f862305b308e6948acadbf80f
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2024-05-28 10:05:19 +0100

    make run_onchange_after-dconf more robust

--- home/.chezmoiscripts/run_onchange_after-dconf.sh
+++ home/.chezmoiscripts/run_onchange_after-dconf.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-for FILE in "$HOME"/.config/dconf/user.d/*; do
-    echo "Loading dconf settings from $FILE"
-    dconf load / < "$FILE"
-done
+DIR="${XDG_CONFIG_HOME:-$HOME/.config}/dconf/user.d"
+
+if [ -d "$DIR" ]; then
+    for FILE in "$DIR"/*; do
+        if [ -r "$FILE" ]; then
+            echo "Loading dconf settings from $FILE"
+            dconf load / < "$FILE"
+        fi
+    done
+fi
