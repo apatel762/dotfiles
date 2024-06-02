94adc88ffba0a83e021148983610a9627a30b0fb
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2024-06-02 10:31:22 +0100

    make the .chezmoiscripts output pretty

--- home/.chezmoiscripts/run_after_everything_setup_dconf.sh
+++ home/.chezmoiscripts/run_after_everything_setup_dconf.sh
@@ -3,13 +3,5 @@
 # shellcheck source=home/private_dot_local/bin/executable_bash_common
 source "${HOME_ORIGINAL:-$HOME}"/.local/bin/bash_common
 
-DIR="${XDG_CONFIG_HOME:-$HOME/.config}/dconf/user.d"
-
-if [ -d "$DIR" ]; then
-    for FILE in "$DIR"/*; do
-        if [ -r "$FILE" ]; then
-            log "Loading dconf settings from $FILE"
-            dconf load / < "$FILE"
-        fi
-    done
-fi
+please_install dconf-bootstrap \
+    && dconf-bootstrap
