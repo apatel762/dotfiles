04ba8a2bf9ddbc73e62a5dc609872852030cc8d5
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2025-03-02 20:08:21 +0000

    add non-standard XDG_BIN_HOME to env

--- home/dot_bashrc.d/05-XDG.sh
+++ home/dot_bashrc.d/05-XDG.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+export XDG_BIN_HOME="${XDG_CACHE_HOME:="$HOME/.local/bin"}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:="$HOME/.local/share"}"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:="$HOME/.config"}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:="$HOME/.local/state"}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:="$HOME/.cache"}"
 
 # ensure that the folders exist
+mkdir -p "$XDG_BIN_HOME"
 mkdir -p "$XDG_DATA_HOME"
 mkdir -p "$XDG_CONFIG_HOME"
 mkdir -p "$XDG_STATE_HOME"
