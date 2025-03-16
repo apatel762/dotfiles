500ec9da5336422effbd386b6b566453379becfe
Arjun Patel <relay-git-1LKQ1GqLp0Nn4@aspatel.com>
2025-03-16 12:22:40 +0000

    fix incorrect cache dir!!!
    it broke uv

--- home/dot_bashrc.d/05-XDG.sh
+++ home/dot_bashrc.d/05-XDG.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export XDG_BIN_HOME="${XDG_CACHE_HOME:="$HOME/.local/bin"}"
+export XDG_BIN_HOME="$HOME/.local/bin"
 export XDG_DATA_HOME="${XDG_DATA_HOME:="$HOME/.local/share"}"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:="$HOME/.config"}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:="$HOME/.local/state"}"
